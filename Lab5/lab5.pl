9e21d10ef1e67d83b7b01dab8a87d33ec60de4a2
Oluwatobiloba Ogunbi <tobiogunbi@gmail.com>
2024-11-14 18:27:31 -0500

    TaskA done

--- Lab5/lab5.pl
+++ Lab5/lab5.pl
@@ -1,2 +1,20 @@
 % Task A
-% Base case
+% Base case for 0
+factorial(0, Acc, Acc)
+
+% invalid input base case
+factorial(N, _, 'Invalid'):-
+    N < 0.
+
+% recursive case
+factorial(N, Acc, Result):-
+    N > 0,
+    NewN is N - 1,
+    NewAcc is Acc * N,
+    factorial(NewN, NewAcc, Result).
+
+%validation
+factorial(N, Acc, Result):-
+    \+ integer(N),
+    Result = 'Invalid'.
+    
